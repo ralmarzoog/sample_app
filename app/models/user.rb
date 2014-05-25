2c4bc46b04b839eac740249ceab1c63d3b70a159
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2014-05-25 12:14:50 -0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    before_save { self.email = email.downcase }
+    before_create :create_remember_token
     has_secure_password
     before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50 }
@@ -7,4 +9,18 @@ class User < ActiveRecord::Base
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: {case_sensitive: false }
     validates :password, length: { minimum: 6 }
+
+    def User.new_remember_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def User.digest(token)
+        Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+        def create_remember_token
+            self.remember_token = User.digest(User.new_remember_token)
+        end
+
 end
