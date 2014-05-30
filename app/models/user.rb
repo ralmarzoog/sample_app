f7a2d519307a1c285568d845c9b1ab3698a3c1f0
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2014-05-30 14:53:46 -0400

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
     has_many :microposts, dependent: :destroy
+    has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+    has_many :followed_users, through: :relationships, source: :followed
+    has_many :reverse_relationships, foreign_key: "followed_id",
+                                     class_name: "Relationship",
+                                     dependent: :destroy
+    has_many :followers, through: :reverse_relationships
+
     before_save { self.email = email.downcase }
     before_create :create_remember_token
+
     has_secure_password
     before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50 }
@@ -10,6 +18,7 @@ class User < ActiveRecord::Base
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: {case_sensitive: false }
     validates :password, length: { minimum: 6 }
+
 
     def User.new_remember_token
         SecureRandom.urlsafe_base64
@@ -20,8 +29,19 @@ class User < ActiveRecord::Base
     end
 
     def feed
-        # This is preliminary.
-        Micropost.where("user_id = ?", id)
+        Micropost.from_users_followed_by(self)
+    end
+
+    def following?(other_user)
+        relationships.find_by(followed_id: other_user.id)
+    end
+
+    def follow!(other_user)
+        relationships.create!(followed_id: other_user.id)
+    end
+
+    def unfollow!(other_user)
+        relationships.find_by(followed_id: other_user.id).destroy
     end
 
     private
